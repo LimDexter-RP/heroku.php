a87ebe48b64e7e7de2490e28622d20def3f70cae
Lim Dexter <20039529@myrp.edu.sg>
2022-07-04 12:39:01 +0800

    V2

--- web/test.php
+++ web/test.php
@@ -1,6 +1,9 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <title>Heroku Test</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Heroku Test V1">
 </head>
 <body>
   Hello World!
